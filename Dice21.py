d311ddad390643d68991d3bbcde7f696c5e92d4a
RaelRo <67835815+RaelRo@users.noreply.github.com>
2020-07-06 11:10:19 +0200

    Add files via upload

--- Dice21.py
+++ Dice21.py
@@ -1,168 +1,65 @@
 import random
 
-print("\n \t Dice21 \n\n Throw the dice against the machine! \n The competitor with the least numeric differenz to 21 wins. Sums greater than 21 lose. \n\n")
+print("\n \t Dice21 \n\n Throw the dice against the machine! \n The competitor with the least numeric difference to 21 wins. Sums greater than 21 lose. \n\n")
 
 dice_comp=0
 dice_hum=0
 roc_max=0
 roh_max=0
-a_list=[]
+value=0
 y=['y']
-n=['n']
-z=0
 se=int(6)
 fue=int(5)
 einund=int(21)
 
-for i in range(100):
+def ran_roll ():
     value=random.randint(1,6)
-    s=str(value)
-    a_list.append(s)
-    
-    
-dice_hum+=int(a_list.pop())
-print("Human's current sum:", dice_hum)
-dice_comp+=int(a_list.pop())
-print("Machine's current sum:", dice_comp)
-print("__________________________")
-dice_comp+=int(a_list.pop())
-dice_hum+=int(a_list.pop())
-print("Human's current sum:", dice_hum)
-print("Machine's current sum:", dice_comp)
-print("__________________________")
-dice_comp+=int(a_list.pop())
-dice_hum+=int(a_list.pop())
-print("Human's current sum:", dice_hum)
-print("Machine's current sum:", dice_comp)
-print("__________________________")
-#human interaction begins
-s=input('Does the human want to roll the dice again? (y/n):')
-roh1=dice_hum
-if s in y:
-    dice_hum+=int(a_list.pop())
+    return value;
+
+for i in range(3):
+    dice_hum+=ran_roll()
+    dice_comp+=ran_roll()
     print("Human's current sum:", dice_hum)
-else:
-    print("Human is on hold")
-    roh_max+=roh1
-    
-roc1=dice_comp
-if (einund-roc1)>=fue and dice_hum<=einund and roc1<einund:
-    dice_comp+=int(a_list.pop())
     print("Machine's current sum:", dice_comp)
     print("__________________________")
-else:
-    roc_max+=roc1
-    print("Machine is on hold")
-    print("__________________________")
-    
 
-#Round_2
-roh2=dice_hum
-if roh_max<=0 :
-    s=input('Does the human want to roll the dice again? (y/n):')
-    if s in y:
-        dice_hum+=int(a_list.pop())
-        print("Human's current sum:", dice_hum)
+#human interaction begins
+while dice_comp<=einund and dice_hum<=einund and roh_max==0 or roc_max==0:
+    if roh_max==0:
+        s=input("Does the human want to roll the dice again? (y/n):")
+        if s in y:
+            dice_hum+=ran_roll()
+            print("Human's current sum:", dice_hum)
+            if dice_hum>=einund:
+                roh_max+=dice_hum
+        else:
+            print("Human is on hold")
+            roh_max+=dice_hum
     else:
         print("Human is on hold")
-        roh_max+=roh2
-else:
-    print("")
-    
-roc2=dice_comp
-if roc_max is z and dice_hum<=einund and roc2<einund and (einund-roc2)>=fue:
-    dice_comp+=int(a_list.pop())
-    print("Machine's current sum:", dice_comp)
-    print("__________________________")
-else:
-    roc_max=roc2
-    if roc1 is not roc2:
+        
+        
+    if roc_max==0 and dice_hum<=einund and dice_comp<einund and (einund-dice_comp)>=fue:
+        dice_comp+=ran_roll()
+        print("Machine's current sum:", dice_comp)
+        print("__________________________")
+    elif roc_max==0 and roh_max!=0 and dice_hum<=einund and roh_max>dice_comp and dice_comp<einund:
+        dice_comp+=ran_roll()
+        print("Machine's current sum:", dice_comp)
+        print("__________________________")
+        
+    else:
+        roc_max=dice_comp
         print("Machine is on hold")
         print("__________________________")
-    else:
-        print("")
 
-#Round_3
-roh3=dice_hum
-if roh_max<=0 :
-    s=input('Does the human want to roll the dice again? (y/n):')
-    if s in y:
-        dice_hum+=int(a_list.pop())
-        print("Human's current sum:", dice_hum)
-    else:
-        print("Human is on hold")
-        roh_max+=roh3
-else:
-    print("")
-    
-roc3=dice_comp
-if roc_max is z and roh_max<=einund and roc3<einund and ((einund-roc3)+(dice_hum-roc3))>=se and roc3<=dice_hum:
-    dice_comp+=int(a_list.pop())
-    print("Machine's current sum:", dice_comp)
-    print("__________________________")
-else:
-    roc_max=roc3
-    if roc2 is not roc3:
-        print("Machine is on hold")
-        print("__________________________")
-    else:
-        print("")
-    
-#Round_4
-roh4=dice_hum
-if roh_max<=0 :
-    s=input('Does the human want to roll the dice again? (y/n):')
-    if s in y:
-        dice_hum+=int(a_list.pop())
-        print("Human's current sum:", dice_hum)
-    else:
-        print("Human is on hold")
-        roh_max+=roh4
-else:
-    print("")
-    
-roc4=dice_comp
-if roc_max is z and roh_max<=einund and roc4<einund and (einund-roc4)>=se and roc4<dice_hum:
-    dice_comp+=int(a_list.pop())
-    print("Machine's current sum:", dice_comp)
-    print("__________________________")
-else:
-    roc_max=roc4
-    if roc3 is not roc4:
-        print("Machine is on hold")
-        print("__________________________")
-    else:
-        print("")
-    
-   
-#Round_5
-roh5=dice_hum
-if roh_max<=0 :
-    s=input('Does the human want to roll the dice again? (y/n):')
-    if s in y:
-        dice_hum+=int(a_list.pop())
-        print("Human's current sum:", dice_hum)
-        roh_max=dice_hum
-    else:
-        print("Human is on hold")
-        roh_max=roh5
-else:
-    print("")
-    
-roc5=dice_comp
-if roc_max is z and roh_max<=einund and roc5<einund and (einund-roc5)>=se and roc5<=dice_hum:
-    dice_comp+=int(a_list.pop())
-    print("Machine's current sum:", dice_comp)
-    print("__________________________")
+
+if roh_max==0 and dice_hum>=einund:
+    roh_max=dice_hum
+elif roc_max==0 and dice_comp>=einund:
     roc_max=dice_comp
-else:
-    roc_max=roc5
-    if roc4 is not roc5:
-        print("Machine is on hold")
-    else:
-        print("")
-    
-
+            
+#Game ends with results
 print("__________________________")
 print("Your sum:", roh_max)    
 print("The machine's sum:", roc_max)
@@ -171,23 +68,23 @@ print("\n")
 print("==========================")
 
 if roh_max>einund and roc_max>einund:
-    print("No winner.")
+    print("\t No winner.")
+    exit
 elif roh_max>einund:
-    print("The machine won!")
+    print("    The machine won!")
     exit
 elif roc_max>einund:
-    print("You won!")
+    print("\t You won!")
     exit
 elif roh_max>roc_max:
-    print("You won!")
+    print("\t You won!")
     exit
 elif roh_max==roc_max:
-    print("Draw!")
+    print("\t Draw!")
     exit
 else:
-    print("The machine won!")
+    print("    The machine won!")
     exit
 
 print("==========================")
-
 
